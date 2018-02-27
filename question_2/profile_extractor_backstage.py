7288d01a832ac006c036f9306fab4a9552dd5b00
Dan Aronson <dan.ar27@gmail.com>
2018-02-27 13:59:13 +0200

    Added test_profileExtractorBackstage

--- question_2/profile_extractor_backstage.py
+++ question_2/profile_extractor_backstage.py
@@ -1,19 +1,25 @@
+from json import JSONDecodeError
+
 from question_2.profile_extractor import ProfileExtractor
 import json
 
 
 class ProfileExtractorBackstage(ProfileExtractor):
-    def __init__(self, html_page):
+    def __init__(self, backstage_html_page):
         self.profile_json = None
         self.education_json = None
-        super().__init__(html_page)
+        super().__init__(backstage_html_page)
+
+    def validate_template(self):
+        self.parse_js()
+        if self.profile_json and self.profile_json['display_name']:
+            self.is_valid = True
 
     def set_skills(self):
         for profile_item in self.profile_json['skills']:
             self.skills.append(profile_item['name'])
 
     def set_name(self):
-        self.parse_js()
         self.name = self.profile_json['display_name']
 
     def set_education(self):
@@ -29,24 +35,28 @@ class ProfileExtractorBackstage(ProfileExtractor):
         data_array = data.split("\n")
         cleaned_profile_object = data_array[6][:len(data_array[6]) - 1].lstrip("profile_data = ")
         cleaned_education_object = data_array[10][:len(data_array[10]) - 1].lstrip("education_data = ")
-        json_object_profile = json.loads(cleaned_profile_object)
-        json_object_education = json.loads(cleaned_education_object)
-        self.profile_json = json_object_profile
-        self.education_json = json_object_education
+        try:
+            json_object_profile = json.loads(cleaned_profile_object)
+            json_object_education = json.loads(cleaned_education_object)
+            self.profile_json = json_object_profile
+            self.education_json = json_object_education
+        except JSONDecodeError:
+            self.is_valid = False
 
 
-html_page = '/home/home/environments/pipl_test/samples/backstage/Amanda Forstrom - Professional Profile, Photos, and Video Reels on Backstage -.html'
+
+# html_page = '/home/home/environments/pipl_test/samples/backstage/Amanda Forstrom - Professional Profile, Photos, and Video Reels on Backstage -.html'
 # html_page = '/home/home/environments/pipl_test/samples/backstage/Joshua Packard - Professional Profile, Photos, and Video Reels on Backstage -.html'
 # html_page = '/home/home/environments/pipl_test/samples/backstage/Mark J. Quiles - Professional Profile, Photos, and Video Reels on Backstage -.html'
-
-soup = ProfileExtractorBackstage(html_page)
-
-print("Printing Name:", soup.get_name())
-
-print("Printing Skills:")
-for item in soup.skills:
-    print(item)
-
-print("Printing Education:")
-for item in soup.education:
-    print(item)
+#
+# soup = ProfileExtractorBackstage(html_page)
+#
+# print("Printing Name:", soup.get_name())
+#
+# print("Printing Skills:")
+# for item in soup.skills:
+#     print(item)
+#
+# print("Printing Education:")
+# for item in soup.education:
+#     print(item)
--- question_2/test_profileExtractorBackstage.py
+++ question_2/test_profileExtractorBackstage.py
@@ -1,15 +1,103 @@
 from unittest import TestCase
 
+from question_2.profile_extractor_backstage import ProfileExtractorBackstage
+
 
 class TestProfileExtractorBackstage(TestCase):
-    def test_set_skills(self):
-        self.fail()
+    def setUp(self):
+        self.backstage_profile_html_ok_1 = ProfileExtractorBackstage(
+            '/home/home/environments/pipl_test/samples/backstage/Amanda Forstrom - Professional Profile, Photos, and Video Reels on Backstage -.html')
+        self.backstage_profile_html_ok_2 = ProfileExtractorBackstage(
+            '/home/home/environments/pipl_test/samples/backstage/Joshua Packard - Professional Profile, Photos, and Video Reels on Backstage -.html')
+
+        self.backstage_profile_html_ok_3 = ProfileExtractorBackstage(
+            '/home/home/environments/pipl_test/samples/backstage/Mark J. Quiles - Professional Profile, Photos, and Video Reels on Backstage -.html')
+
+        self.backstage_profile_html_no_name = ProfileExtractorBackstage(
+            '/home/home/environments/pipl_test/samples/backstage/no_name.html')
+
+    def test_validate_template(self):
+        self.assertTrue(self.backstage_profile_html_ok_1.is_valid)
+        self.assertTrue(self.backstage_profile_html_ok_2.is_valid)
+        self.assertTrue(self.backstage_profile_html_ok_3.is_valid)
+        self.assertFalse(self.backstage_profile_html_no_name.is_valid)
 
     def test_set_name(self):
-        self.fail()
+        self.assertEqual(self.backstage_profile_html_ok_1.name, 'Amanda Forstrom')
+        self.assertEqual(self.backstage_profile_html_ok_2.name, 'Joshua Packard')
+        self.assertEqual(self.backstage_profile_html_ok_3.name, 'Mark J. Quiles')
+        self.assertIsNone(self.backstage_profile_html_no_name.name)
 
     def test_set_education(self):
-        self.fail()
+        self.assertEqual(self.backstage_profile_html_ok_1.education, [
+            {
+                'years': '1970-01-01T00:00:00',
+                'major': 'MFA Acting',
+                'institution_name': 'University of South Carolina'
+            }
+        ])
+        self.assertEqual(self.backstage_profile_html_ok_2.education, [
+            {'major': '2 week summer acting for film', 'institution_name': 'NY Film Academy',
+             'years': '2017-09-11T14:40:12'},
+            {'major': 'Various acting classes', 'institution_name': "Actor's Technique NY",
+             'years': '2017-02-17T15:35:45'},
+            {'major': 'NA', 'institution_name': 'Barrow Theater School', 'years': '2017-01-24T18:09:59'},
+            {'major': 'NA', 'institution_name': 'The Stage Theater School', 'years': '2017-01-24T18:09:59'},
+            {'major': 'NA', 'institution_name': 'Joyful Singing', 'years': '2017-01-24T18:09:59'},
+            {'major': 'summer class', 'institution_name': 'A Class Act NY', 'years': '2017-01-24T18:09:59'}
+        ])
+        self.assertEqual(self.backstage_profile_html_ok_3.education, [
+            {'institution_name': 'McKenzie/ Delevan Studios', 'major': 'Voice', 'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'Tim Welch Music', 'major': 'Voice', 'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'Coupe Dance Studio', 'major': 'Tap Dance/ Theatre Dance',
+             'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'Willful Productions', 'major': 'Shakespeare Acting & Private Coaching',
+             'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'The Real Stage', 'major': 'Acting', 'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'Montclair State University', 'major': 'M.A. Education Admin. & Educ. Trainer',
+             'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'William Paterson University', 'major': 'M. Ed. - Elem. Ed./ Bilingual Ed.',
+             'years': '1970-01-01T00:00:00'},
+            {'institution_name': 'Calvin College', 'major': 'B. A. - Theatre/ Telecommunications',
+             'years': '1970-01-01T00:00:00'}
+        ])
+        self.assertEqual(self.backstage_profile_html_no_name.education, [])
 
-    def test_parse_js(self):
-        self.fail()
+    def test_set_skills(self):
+        self.assertEqual(self.backstage_profile_html_ok_1.skills, [
+            'Music/Musician',
+            'Accents/Dialects',
+            'Fight Training',
+            'Horseback Riding',
+            'Improvisation',
+            'Firearms Training',
+            'Ballet',
+            'Shakespeare Training',
+            'Clown Training',
+            'Acrobatics',
+            'Sight Reading',
+            'Athletic',
+            'Foreign Language',
+            'Singer',
+            'Lyrical',
+            'Jazz',
+            'Tap',
+            'Pointe',
+            'Basic Ballroom'
+        ])
+        self.assertEqual(self.backstage_profile_html_ok_2.skills, [])
+        self.assertEqual(self.backstage_profile_html_ok_3.skills, [
+            'Bilingual/ Biliterate - English/ Spanish',
+            'Can Drive Stick Shift (Manual)',
+            'Certified School Teacher/ Administrator',
+            'First Aid/ Cpr Trained',
+            'Improvisation',
+            'Lighting Design',
+            'Oil Painting',
+            'Stage Construction/ Carpentry',
+            'Stage/ Prop Design',
+            'Tae Kwon Do',
+            'Tap',
+            'Works Well With Children'
+        ])
+        self.assertEqual(self.backstage_profile_html_no_name.skills, [])
